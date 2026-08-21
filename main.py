5d1a2a79c82146ede0d9b0d9f9caef38d8791098
defallier <defallier.torn@gmail.com>
2026-08-21 21:42:11 +0100

    0.04 - You can move around now

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
+from asteroids import *
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -14,6 +17,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+
+
 
     while True:
         log_state()
@@ -21,8 +36,13 @@ def main():
             if event.type == pygame.QUIT:
                     return
         screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
 
 
 
